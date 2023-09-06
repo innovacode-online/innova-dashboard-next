38800ec3f61baf0dd64277e52663c4f3b69f461d
Mattias <maduarte@innovacode.online>
2023-09-05 20:09:32 -0400

    refactor

--- src/products/components/ProductsListClient.tsx
+++ src/products/components/ProductsListClient.tsx
@@ -2,8 +2,8 @@
 
 import styles from '@/shared/styles/styles.module.css'
 import { useProducts } from '../hooks/useProducts';
-import Image from 'next/image';
-import Link from 'next/link';
+import { ProductCard } from './ProductCard';
+
 
 export const ProductsListClient = () => {
 
@@ -19,29 +19,11 @@ export const ProductsListClient = () => {
         )
     }
 
-    const baseUrl = 'https://store.innovacode.online'
-
     return (
         <div className='product__list'>
             {
                 products?.map( product => (
-                    <div className='product__card'>
-                        <Image
-                            src={ baseUrl + product.images[0].url }
-                            alt={ product.name }
-                            width={ 858 }
-                            height={ 858 }
-                        />
-            
-                        <h3 className='mt-3'>{ product.name }</h3>
-                        <p className='text-slate-500 line-clamp-2'>{ product.description }</p>
-                        <Link
-                            className='btn-primary'
-                            href={`/server-side/product/${ product.slug }`}
-                        >
-                            Ver Producto
-                        </Link>
-                    </div>  
+                    <ProductCard product={ product }/>
                 ))
             }
         </div>
